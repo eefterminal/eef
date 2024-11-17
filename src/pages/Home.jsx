cd724a4ab09e63352a666cf92deb9dd25710565f
eefterminal <eefterminal7@gmail.com>
2024-11-17 16:28:48 -0500

    adwwad

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,10 +9,10 @@ const Home = () => {
     <>
       <div className="home">
         <div className="flex">
-          <a href="https://vvaifu.fun" className="redirect" target="_blank ">
+          <a href="https://vvaifu.fun/character/673a5cb074b9788b3c5d36b3" className="redirect" target="_blank ">
             <img src={vv} alt="vvaifu" />
           </a>
-          <a href="https://pump.fun/board" className="redirect" target="_blank ">
+          <a href="https://pump.fun/coin/9cS2vsANfgEp9urCeDbbmsT7Y9Ci4mMW6JQpnEe4VHGZ" className="redirect" target="_blank ">
             <img src={pump} alt="pumpfun"  className="pump"/>
           </a>
           <a href="https://x.com/eef_terminal" className="redirect" target="_blank ">
